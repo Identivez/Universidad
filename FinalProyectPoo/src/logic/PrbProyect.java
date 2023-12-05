0cc226ca26225af4f2df7dad6410e8a24f58043a
ghael <ghael@STAR-LORD>
2023-12-04 19:27:59 -0600

    Good

---
+++ FinalProyectPoo/src/logic/PrbProyect.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logic;
+import ugi.menu;
+import ugi.ca;
+
+
+public class PrbProyect 
+{
+
+    
+    public static void main(String[] args)
+    {
+       menu m= new menu();
+       m.setVisible(true);
+        m.setUndecorated(true);
+        initComponents();
+        m.setLocationRelativeTo(null);
+        
+        
+        
+        
+    }
+
+    private static void initComponents()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+   
+
+}
